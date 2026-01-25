a2b3fded4c48bac23a604da47110a7570eb1a05c
zyrel2002 <87981190+zyrel2002@users.noreply.github.com>
2026-01-26 01:19:12 +0800

    finished student5 - frisco

--- app/src/main/java/com/example/thedreamteamapp/BioStudent5.kt
+++ app/src/main/java/com/example/thedreamteamapp/BioStudent5.kt
@@ -1,4 +1,30 @@
 package com.example.thedreamteamapp
 
-class BioStudent5 {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+
+
+class BioStudent5: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentScreen(
+                        profileImageRes = R.drawable.student5,
+                        studentName = "Mary Mae L. Frisco",
+                        bio = "Dangal Greetings! I am a student studying Information Technology" +
+                                " . I enjoy designing digital solutions.",
+                        onBackClick = { finish() }
+                    )
+                }
+            }
+        }
+    }
 }
