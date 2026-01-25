51793bac3a64169eaf491c7e8b6855427d0e2932
euniels <ooeangelo@gmail.com>
2026-01-25 19:56:31 +0800

    Finished student bio screen 4 (Guarte)

--- app/src/main/java/com/example/thedreamteamapp/BioStudent4.kt
+++ app/src/main/java/com/example/thedreamteamapp/BioStudent4.kt
@@ -1,4 +1,30 @@
 package com.example.thedreamteamapp
 
-class BioStudent4 {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+
+
+class BioStudent4: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentScreen(
+                        profileImageRes = R.drawable.student4,
+                        studentName = "Eunel Angelo Guarte",
+                        bio = "Dangal Greetings! I am a student studying Information Technology" +
+                                " . My passions are drawing, gaming, and studying.",
+                        onBackClick = { finish() }
+                    )
+                }
+            }
+        }
+    }
 }
